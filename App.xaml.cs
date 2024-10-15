1aacfe6463d86fb6d4f2f4f1ef28dc9cb0a8c14a
Jonas Heilemann <134715426+JonHeil@users.noreply.github.com>
2024-10-15 22:41:53 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TopCompetitor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopCompetitor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
